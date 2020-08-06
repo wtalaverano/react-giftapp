fe06006c7a05c0de7425468e4cb38514b423ffc5
Walter Talaverano <waltertalaverano@hotmail.com>
2020-08-06 01:01:59 -0500

    Initial Commit

---
+++ docs/precache-manifest.500cd9ba5f84c1fd911e634e01d99ac6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc4ceac35ffa3ed858a02a183b8baf8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "acedc692118c4e60fe15",
+    "url": "/static/css/main.07179a7c.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acedc692118c4e60fe15",
+    "url": "/static/js/main.65185f9c.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
